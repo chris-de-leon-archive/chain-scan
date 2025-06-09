afeced719dea22aa6a4087b8fa257c88af82fcde
chris-de-leon <135092418+chris-de-leon@users.noreply.github.com>
2025-06-09 09:45:34 +0000

    chore: migrate to bitwarden

--- README.md
+++ README.md
@@ -1,9 +1,66 @@
-# chain-scan
+# Chain Scan
 
-To install dependencies:
+## Archive Notice
 
-```bash
-bun install
-```
+Chain Scan is a universal block explorer for both EVM and non-EVM chains. Users can log into the app, connect to one or more chains, and explore both the block and transaction data for the chains that they connected to. There is also a corresponding CLI tool provided as well in case users prefer more of a lower-level experience. While there are several parts missing, the app comes with authentication baked in and out-of-the-box support for several popular EVM and non-EVM chains. There were also thoughts of eventually adding a dashboard feature as well so that users can create their own visualizations (similar to Grafana but geared more towards blockchain).
 
-This project was created using `bun init` in bun v1.2.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+For now, I've decided to pause development on this project due to work. This app was mostly meant to be an experiment with some new tools that I've been meaning to learn: BetterAuth, Svelte, and Bun. The overall developer experience was very pleasant for all these tools and I can see myself using both of them more in the future.
+
+## Setup
+
+1. Enter a Nix shell
+
+   ```sh
+   nix develop
+   ```
+
+1. Install dependencies
+
+   ```sh
+   bun install
+   ```
+
+## CLI
+
+1. Navigate to `./packages/apps/cli`
+
+1. The CLI can be invoked using:
+
+   ```sh
+   bun run dev
+   ```
+
+1. A production build can be created using:
+
+   ```sh
+   bun run build
+   ```
+
+## Web UI
+
+1. Navigate to `./packages/apps/web`
+
+1. Create a `.env` file with the following format:
+
+   ```text
+   PUBLIC_BETTER_AUTH_URL="http://127.0.0.1:5173"
+   BETTER_AUTH_SECRET="..."
+   DATABASE_URL="local.db"
+   CACHE_NAME="chain-scan"
+   CACHE_URL="local.cache"
+   CACHE_LIM=1000
+   ```
+
+1. Setup the DB:
+
+   ```sh
+   bun run db:init
+   ```
+
+1. Start the app:
+
+   ```sh
+   bun run dev --host=0.0.0.0
+   ```
+
+1. Make an account and sign into the app to explore
--- flake.nix
+++ flake.nix
@@ -1,37 +1,44 @@
-# https://github.com/NixOS/nixpkgs/commit/5996243e793c19a0933c8df5869b79088279343d
 {
   inputs = {
-    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/5996243e793c19a0933c8df5869b79088279343d.tar.gz";
-    secrets.url = "https://github.com/chris-de-leon/secrets-cli/archive/refs/tags/v1.2.2.tar.gz";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, secrets, utils }:
+  outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              scli = secrets.defaultPackage.${prev.system};
-            })
-          ];
-        };
+        pkgs = import nixpkgs { inherit system; };
+        bws = import ./nix/bws { inherit pkgs; };
       in
       rec {
         formatter = pkgs.nixpkgs-fmt;
 
         devShells.default = pkgs.mkShell {
+          # NOTE: it is much faster to install the Bitwarden secrets CLI
+          # via Docker instead of Nix. Installing via Docker also allows
+          # us to avoid having to use the NIXPKGS_ALLOW_UNFREE=1 env var
+          # and `--impure` for Nix shells
           packages = [
             pkgs.bashInteractive
-            pkgs.lastpass-cli
             pkgs.sqlite
             pkgs.nodejs
-            pkgs.scli
             pkgs.bun
+            bws
           ];
 
+          # NOTE: `bws secret list` will NOT return an empty list if the project contains no secrets. Instead it will fail with a 404 error. To fix
+          # this, there needs to be at least one dummy secret added to the project. There's a thread about this linked below but it has no activity
+          # https://community.bitwarden.com/t/bws-secret-list-project-id-shouldnt-return-a-404-if-there-are-no-secrets-in-the-project/64668
           shellHook = ''
+            export BWS_PROJECT_ID=""
+            if [[ -f ./.bws ]]; then
+              BWS_PROJECT_ID="$(cat .bws)"
+            else
+              BWS_PROJECT_ID="$(bws project create 'chain-scan' | jq -erc '.id')"
+              bws secret create 'PROJECT_ID' "$BWS_PROJECT_ID" "$BWS_PROJECT_ID"
+              echo -n "$BWS_PROJECT_ID" > .bws
+            fi
+
             # HACK: this is just a short term solution to get lmdb working - need to find a better way to add it to the shell env:
             # https://discourse.nixos.org/t/nixos-with-poetry-installed-pandas-libstdc-so-6-cannot-open-shared-object-file/8442/9
             export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
